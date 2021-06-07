88a7b51eac4935514f93fc916c5096ca3bcc9f6f
bhupathi08 <krishna.bhupathi92@gmail.com>
2021-06-07 23:48:36 +0530

    Delete src directory

--- src/main/java/gov/census/maftigerstats/config/MessageSecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package gov.census.maftigerstats.config;
-
-import org.springframework.security.web.context.*;
-
-public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
